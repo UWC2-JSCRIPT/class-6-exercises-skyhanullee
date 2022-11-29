abccab2390cf4f293ed0b4ef8a67ddce4e744fb5
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2022-11-29 04:22:24 -0800

    completed exercises

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,37 @@
-// Create a new <a> element containing the text "Buy Now!"
-// with an id of "cta" after the last <p>
+$(document).ready(function() {
+    // Create a new <a> element containing the text "Buy Now!"
+    // with an id of "cta" after the last <p>
+    const $newA = $('<a>');
+    $newA.text('Buy Now!')
+    $newA.attr('id', 'cta');
 
-// Access (read) the data-color attribute of the <img>,
-// log to the console
+    // const $lastP = $('p');
+    // $lastP.append($newA);
+    $('main').append($newA);
 
-// Update the third <li> item ("Turbocharged"),
-// set the class name to "highlight"
+    // Access (read) the data-color attribute of the <img>,
+    // log to the console
+    const $img = $('img');
+    const $imgDataColor = $img.attr('data-color');
+    console.log($imgDataColor);
 
-// Remove (delete) the last paragraph
-// (starts with "Available for purchase now…")
+    // Update the third <li> item ("Turbocharged"),
+    // set the class name to "highlight"
+    const $thirdLi = $('ul li:nth-child(3)');
+    $thirdLi.toggleClass('highlight');
 
-// Create a listener on the "Buy Now!" link that responds to a click event.
-// When clicked, the the "Buy Now!" link should be removed
-// and replaced with text that says "Added to cart"
+
+    // Remove (delete) the last paragraph
+    // (starts with "Available for purchase now…")
+    $('p').remove();
+
+    // Create a listener on the "Buy Now!" link that responds to a click event.
+    // When clicked, the "Buy Now!" link should be removed
+    // and replaced with text that says "Added to cart"
+    $newA.click((e) => {
+        // console.log('buy now clicked');
+        $newA.removeAttr('href');
+        $newA.text('Added to cart');
+    });
+
+});
