d3ac569dab9257f2ea6af06fc315783c741c3e9d
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2022-11-29 16:06:31 -0800

    completed the exercises

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,6 +5,15 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+$('#more-info').click((e) => {
+  console.log('more-info clicked');
+  e.preventDefault();
+  alert(`Here's some info`);
+});
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+$('#dog').click((e) => {
+  e.stopPropagation();
+  alert(`Bow wow!`);
+});
